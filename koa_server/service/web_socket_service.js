76d0728a16ec45f88913b2c5ce38c1c05effb951
Evildoer98 <1213089093@qq.com>
2021-06-20 14:57:47 +0800

    v4

--- koa_server/service/web_socket_service.js
+++ koa_server/service/web_socket_service.js
@@ -16,22 +16,29 @@ module.exports.listen = () => {
     // msg：由客户端发给服务器的数据
     client.on('message', async msg => {
         console.log('客户端发送数据给服务器了: ' + msg);
-        let payload = JSON.parse(msg)
-        const action = payload.action
+        let payload = JSON.parse(msg) // 解析客户端发送来的 json 数据
+        // console.log(payload);
+        const action = payload.action // 查看客户端的意图
         if (action === 'getData') {
             let filePath = '../data/' + payload.charName + '.json'
+            // console.log(filePath);
             // payload.charName // trend seller map rank hot stock
+            // console.log(__dirname);
             filePath = path.join(__dirname, filePath)
+            // console.log(filePath);
             const ret = await fileUtils.getFileJsonData(filePath)
             // 需要在服务端获取到的数据的基础之上，增加一个data 的字段
             // data所对应的值，就是某个json文件的内容
+            // console.log(ret);
             payload.data = ret
+            // console.log(payload);
             client.send(JSON.stringify(payload))
 
         }else{
             // 原封不动的将所接受到的数据转发给每一个处于连接状态的客户端
             // wss.clients // 所有客户端的连接
             wss.clients.forEach(client => {
+                console.log(msg);
                 client.send(msg)
                 console.log('所有数据都来啦');
             })
